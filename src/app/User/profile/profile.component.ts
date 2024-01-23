445c9a92940cd81d8f478d5129a767067a0f1dfa
faizu6302 <faizan@btl.com>
2024-01-23 19:07:29 +0530

    everything upto date

--- src/app/User/profile/profile.component.ts
+++ src/app/User/profile/profile.component.ts
@@ -1,39 +1,26 @@
-import { Component, NgModule, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
-import { AppService } from "src/app/app.service";
-import { login } from "src/app/login";
-import { SharedService } from "src/app/shared/auth.service";
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AppService } from 'src/app/app.service';
+import { login } from 'src/app/login';
+import { SharedService } from 'src/app/shared/auth.service';
 
 @Component({
-    selector:'app-profile',
-    templateUrl:'./profile.component.html',
-    styleUrls:['./profile.component.css'],
-    
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
 })
-
 export class ProfileComponent implements OnInit {
-
   userProfileDetails: login;
   userDetailsForm: FormGroup;
 
   constructor(
     private profileAppService: AppService,
     private profileSharedService: SharedService
-  ) { }
-
+  ) {}
 
   ngOnInit(): void {
 
-    this.formdata()
-  }
-
-  formdata() {
-    this.userProfileDetails = this.profileAppService.predefinedLoginDetails.find((x) => {
-      return x.username === this.profileSharedService.userLoggedInName
-
-    })
-
-    this.userDetailsForm = new FormGroup({
+  this.userDetailsForm = new FormGroup({
       username: new FormControl(''),
       password: new FormControl(''),
       firstname: new FormControl(''),
@@ -42,29 +29,49 @@ export class ProfileComponent implements OnInit {
       mobile: new FormControl(''),
       emailid: new FormControl(''),
       address: new FormControl(''),
-
-    })
-
-    this.userDetailsForm.patchValue(this.userProfileDetails)
-
-    this.userDetailsForm.disable()
-  }
-
-  onSave() {
-    const index = this.profileAppService.predefinedLoginDetails.findIndex((x) => {
-      return x.username === this.profileSharedService.userLoggedInName;
     });
 
-    if (index !== -1) {
-      this.profileAppService.predefinedLoginDetails[index] = this.userDetailsForm.value;
-    }
-    this.formdata()
-    this.userDetailsForm.disable()
+    this.userDetailsForm.disable();
+
+    this.userProfileDetails= this.profileAppService.predefinedLoginDetails.find((x)=>{
+      return x.username === this.profileSharedService.userLoggedInName
+    })
+
+    this.userDetailsForm.patchValue(this.userProfileDetails);
+  }
+
+ 
+
+  onSave() {
+    const savedValue = this.userDetailsForm.value
+
+    this.profileAppService.predefinedLoginDetails.find((x)=>{
+      if(x.username === this.profileSharedService.userLoggedInName){
+        x.address = savedValue.address
+        x.emailid = savedValue.emailid
+        x.firstname = savedValue.firstname
+        x.mobile = savedValue.mobile
+        x.gender = savedValue.gender
+        x.password = savedValue.password
+        x.lastname = savedValue.lastname
+      }
+    })
+
+   
+   
+    this.userDetailsForm.disable();
   }
 
   onEditClicked() {
-    this.userDetailsForm.enable()
+    this.userDetailsForm.get('password').enable();
+    this.userDetailsForm.get('firstname').enable();
+    this.userDetailsForm.get('lastname').enable();
+    this.userDetailsForm.get('gender').enable();
+    this.userDetailsForm.get('mobile').enable();
+    this.userDetailsForm.get('emailid').enable();
+    this.userDetailsForm.get('address').enable();
+
+    // Disable the username field
+    this.userDetailsForm.get('username').disable();
   }
-
-
 }
